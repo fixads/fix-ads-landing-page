69c134dc330abcfc6a57ab3a186b2f237cf59d1a
Timur <misharintimur@gmail.com>
2023-02-07 23:48:26 +0200

    webpack added

--- webpack.config.js
+++ webpack.config.js
@@ -1,57 +1,98 @@
+const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
-const ImageminWebpackPlugin = require('imagemin-webpack-plugin').default;
-const CopyWebpackPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerWebpackPlugin = require('css-minimizer-webpack-plugin');
+const ImageMinimizerPlugin = require("image-minimizer-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   mode: 'production',
+  entry: './src/index.js',
+  output: {
+    filename: '[name].[contenthash].js',
+    path: path.resolve(__dirname, 'dist')
+  },
   module: {
     rules: [
       {
-        test: /\.css$/i,
-        use: [
-          'style-loader',
-          'css-loader',
-        ],
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader']
       },
       {
-        test: /\.(png|webp|ico|txt|xml)$/i,
+        test: /\.(png|jpeg|jpg|gif|svg)$/,
         use: [
           {
             loader: 'file-loader',
-          },
-        ],
-      },
-    ],
+            options: {
+              name: '[name].[ext]',
+              outputPath: './accets/images'
+            }
+          }
+        ]
+      }
+    ]
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html',
+      filename: 'index.html',
       minify: {
-        collapseWhitespace: true,
         removeComments: true,
-        removeRedundantAttributes: true,
-        removeScriptTypeAttributes: true,
-        removeStyleLinkTypeAttributes: true,
-        useShortDoctype: true,
+      }
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].[contenthash].css'
+    }),
+    new CssMinimizerWebpackPlugin({
+      minimizerOptions: {
+        preset: [
+          "default",
+          {
+            discardComments: { removeAll: true },
+          },
+        ],
       },
     }),
-    new CssMinimizerPlugin(),
-    new ImageminWebpackPlugin({
-      test: /\.(png|webp)$/i,
-      plugins: [
-        require('imagemin-pngquant') ({
-          quality: [0.95, 1],
-        })
-      ],
-    }),
-    new CopyWebpackPlugin({
+    new CopyPlugin({
       patterns: [
+        {
+          from: "src/assets/images/*.png",
+          to({ context, absoluteFilename }) {
+            return "assets/images/[name][ext]";
+          },
+        },
         { from: 'src/favicon.ico', to: 'favicon.ico' },
         { from: 'src/robots.txt', to: 'robots.txt' },
         { from: 'src/sitemap.xml', to: 'sitemap.xml' },
-        { from: 'src/images', to: 'images'},
       ],
     }),
   ],
+  optimization: {
+    minimizer: [
+      "...",
+      new CssMinimizerWebpackPlugin(),
+      new ImageMinimizerPlugin({
+        minimizer: {
+          implementation: ImageMinimizerPlugin.imageminMinify,
+          options: {
+            plugins: [
+              "imagemin-gifsicle",
+              "imagemin-mozjpeg",
+              "imagemin-pngquant",
+              "imagemin-svgo",
+            ],
+          },
+        },
+        // generator: [
+        //   {
+        //     type: "asset",
+        //     implementation: ImageMinimizerPlugin.imageminGenerate,
+        //     options: {
+        //       plugins: ["imagemin-webp"],
+        //     },
+        //   },
+        // ],
+      }),
+    ],
+  },
 };
